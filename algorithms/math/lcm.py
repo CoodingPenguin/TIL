894903f4ae7d7b76677c14724c3b53b8e895c358
CoodingPenguin <unodostre@naver.com>
2021-01-01 22:15:52 +0900

    study: ✍ add lcm code (first commit of 2021!)

---
+++ algorithms/math/lcm.py
@@ -0,0 +1,16 @@
+# title: 최소공배수
+# src: https://www.acmicpc.net/problem/2609
+# time: O(logN)
+
+def lcm(x, y):
+    '''
+    math 모듈의 최대공약수를 이용해 최소공배수를 계산
+    - `x`, `y`: 최소공배수를 구하고자 하는 두 수
+    '''
+    import math
+    return x * y // math.gcd(x, y)
+
+
+if __name__ == "__main__":
+    x, y = 10, 24
+    print(lcm(x, y))
